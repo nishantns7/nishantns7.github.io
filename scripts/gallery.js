9d9abe45ef3cbfe26e88083059813c0aeede7118
Nishant <nishant.s@zemosolabs.com>
2018-08-28 16:16:35 +0530

    Admin

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -12,14 +12,37 @@ function populatePage(jsonObj) {
 	var srcList = jsonObj["src"];
 	for (var i = 0; i < srcList.length; i++) {
 		var myImg = document.createElement('img');
+		var editImg = document.createElement('img');
+		var closeImg = document.createElement('img');
 		var myLink = document.createElement('a');
+
+		editImg.className = "edit";
+		editImg.src = "images/edit_button.svg";
+
+		closeImg.className = "close";
+		closeImg.src = "images/close_button.png";
+
 		myImg.src = srcList[i];
 		myLink.href = srcList[i];
+
 		myImg.height = "300";
 		myImg.width = "400";
+
 		myLink.target = "_blank";
 		myLink.className = "gallery";
+
+		myImg.appendChild(closeImg);
+		myImg.appendChild(editImg);
 		myLink.appendChild(myImg);
 		myDiv.appendChild(myLink);
+
+		myImg.onmouseover = function() {
+			editImg.style.display = "inline-block";
+			closeImg.style.display = "inline-block";
+		}
+		myImg.onmouseout = function() {
+			editImg.style.display = "none";
+			closeImg.style.display = "none";
+		}
 	}
 }
